59bca01d84d92576c7f9273f4f96174675703430
luigi.zuniga-innova-ti.cl <luis.zuniga@innova-ti.cl>
2019-10-02 00:55:31 -0300

    API WEB CON ASP.NET Core

---
+++ CRUDNET/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CRUDNET.Models
+{
+    public class TodoItem
+    {
+
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public bool IsComplete { get; set; }
+
+    }
+}
